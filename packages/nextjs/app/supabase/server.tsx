111b311e160c70d4b6313f89a26cc9bf6e013202
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2024-10-04 22:31:01 +0200

    Update ENV vars

--- packages/nextjs/app/supabase/server.tsx
+++ packages/nextjs/app/supabase/server.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const createSupabaseClient = () => {
-  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+  return createClient(process.env.NEXT_SUPABASE_URL!, process.env.NEXT_SUPABASE_ANON_KEY!);
 };
