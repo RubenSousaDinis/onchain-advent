e8ae497fd0df7ae778d74bd4fde32724d72aed4f
Panos Matsinopoulos <panos@mixlr.com>
2024-10-05 20:10:35 +0300

    Fix missing frames metadata

--- packages/nextjs/constants.ts
+++ packages/nextjs/constants.ts
@@ -1,10 +1,14 @@
 import { getEnvVariable } from "./utils/env";
 
-export const BASE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+export const BASE_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : `http://localhost:${process.env.PORT || 3000}`;
+
 export const DEPLOYED_CONTRACTS_JSON_RPC_URL = getEnvVariable(
   "DEPLOYED_CONTRACTS_JSON_RPC_URL",
   "https://base-sepolia-rpc.publicnode.com"
 );
+
 export const PRIVY_APP_ID = getEnvVariable("NEXT_PRIVY_APP_ID");
 export const PRIVY_SECRET = getEnvVariable("NEXT_PRIVY_SECRET");
 export const SUPABASE_URL = getEnvVariable("NEXT_SUPABASE_URL");
